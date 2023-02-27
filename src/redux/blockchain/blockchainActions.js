0c875a4d52ead5978f3cd5b49157f73a93468d18
Ali Solanki <alisolanki123@gmail.com>
2023-02-27 18:16:44 +0530

    updated code 🚀

--- src/redux/blockchain/blockchainActions.js
+++ src/redux/blockchain/blockchainActions.js
@@ -60,7 +60,7 @@ export const connect = () => {
         const networkId = await ethereum.request({
           method: "net_version",
         });
-        if (networkId == CONFIG.NETWORK.ID) {
+        if (networkId === CONFIG.NETWORK.ID) {
           const SmartContractObj = new Web3EthContract(
             abi,
             CONFIG.CONTRACT_ADDRESS
